2d3d91a285107b634fc21c758eb364b53ac453f6
임소리 <uiwhewi@gmail.com>
2025-08-06 22:30:04 +0900

    [Bronze I] Title: 소인수분해, Time: 24 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+    int n;
+    scanf("%d", &n);
+
+    int i=2; 
+
+    while (n>1) {
+        if (n % i == 0){   
+            printf("%d\n", i);  
+            n =n/i;          
+        } 
+        else{
+            i++;  
+        }
+    }
+}
