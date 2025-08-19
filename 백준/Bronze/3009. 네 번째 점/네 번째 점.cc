c4f57a2885269e62ba420232e3a4d4e8261f9205
임소리 <uiwhewi@gmail.com>
2025-08-20 00:52:44 +0900

    [Bronze III] Title: 네 번째 점, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.cc
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+    int x1,y1,x2,y2,x3,y3;
+    scanf("%d %d %d %d %d %d",&x1,&y1,&x2,&y2,&x3,&y3);
+
+    int x4, y4;
+
+    // x좌표 찾기
+    if(x1==x2) x4 = x3;
+    else if(x1==x3) x4 = x2;
+    else x4 = x1;
+
+    // y좌표 찾기
+    if(y1==y2) y4 = y3;
+    else if(y1==y3) y4 = y2;
+    else y4 = y1;
+
+    printf("%d %d\n", x4, y4);
+    return 0;
+}
